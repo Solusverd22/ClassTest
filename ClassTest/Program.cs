a54934f461b728e24510a3bc4be21fcd207b8416
Conor Michael Gardiner <GAR40024248@serc.ac.uk>
2017-11-13 12:15:12 +0000

    added some stuff

---
+++ ClassTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Michael = new Rectangle();
+
+            //Michael.width = int.Parse(Console.ReadLine());
+            //Console.Write(Michael.width);
+            //Console.Read();
+
+
+
+        }
+    }
+}
---
+++ ClassTest/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTest
+{
+    class Rectangle
+    {
+        private int _width;
+        private int _height;
+
+        public int width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+    }
+}
